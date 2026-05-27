32dbe6e69fb6ad4fff5cca024582d3d27516f345
amrsan <amribrahim924@gmail.com>
2026-05-27 09:18:49 +0300

    Restore tailwind plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/Insta-admin-new/',
-});
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    // If you are in 'build' mode, use the repo name. 
+    // If you are in 'serve' mode (npm run dev), use '/'
+    base: command === 'build' ? '/Insta-admin-new/' : '/',
+  }
+})
